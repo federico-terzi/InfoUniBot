f643b95908d399b97c46dc0d64c9be5351252ad8
max1808 <massimo.schembri18@gmail.com>
2017-09-27 15:54:28 +0200

    Cambiata api telepot->telebot

--- bot.py
+++ bot.py
@@ -1,32 +1,44 @@
 # coding=utf-8
-import telepot
-from telepot.loop import MessageLoop
-import time
+import telebot
 import os
-import csv
 import infounibot.util as util
 
 
 # Get the bot token fron the enviromental variables
 BOT_TOKEN = os.environ["bot_token"]
-
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type, chat_type, chat_id)
-    #riconoscimento input
-    if content_type == 'text' and  msg['text'] == '/start':
-        util.add_id(chat_id)
-
-
-
-bot = telepot.Bot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
 
 str_welcome = 'Benvenuto nel fantastico UniBot'
 
-MessageLoop(bot, handle).run_as_thread()
-print ('Listening ...')
+@bot.message_handler(commands=['start'])
+def subscribe(message):
+    id = message.chat.id
+    print(str(id))
+    util.add_id(id)
+    bot.reply_to(message,str_welcome)
+
+
+
+#def handle(msg):
+ #   content_type, chat_type, chat_id = telepot.glance(msg)
+  #  print(content_type, chat_type, chat_id)
+   # #riconoscimento input
+    #if content_type == 'text' and  msg['text'] == '/start':
+     #   util.add_id(chat_id)
+
+
+
+
+print("Polling...")
+
+bot.polling()
+
+
+
+#MessageLoop(bot, handle).run_as_thread()
+#print ('Listening ...')
 
 # Keep the program running.
-while 1:
-    time.sleep(10)
+#while 1:
+ #   time.sleep(10)
 
