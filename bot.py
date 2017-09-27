c03e5091fe3898395bc65afbd85c7184457882d8
max1808 <massimo.schembri18@gmail.com>
2017-09-27 15:20:19 +0200

    Aggiunto l'uso della libreria infounibot/util.py

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from telepot.loop import MessageLoop
 import time
 import os
 import csv
+import infounibot.util as util
+
 
 # Get the bot token fron the enviromental variables
 BOT_TOKEN = os.environ["bot_token"]
@@ -13,26 +15,7 @@ def handle(msg):
     print(content_type, chat_type, chat_id)
     #riconoscimento input
     if content_type == 'text' and  msg['text'] == '/start':
-        print(chat_id)
-        control = True
-        control_file = open("notify.csv","r")
-        control_reader = csv.reader(control_file)
-
-        for row in control_reader :
-            if not control :
-                break
-            print(row)
-            if row == [str(chat_id)] :
-                control = False
-        control_file.close()
-        if control :
-            notify_file = open("notify.csv", "a")
-            writer = csv.writer(notify_file)
-            writer.writerow([str(chat_id)])
-            bot.sendMessage(chat_id, str_welcome)
-            notify_file.close()
-        else:
-            bot.sendMessage(chat_id, "Sei già registrato")
+        util.add_id(chat_id)
 
 
 
