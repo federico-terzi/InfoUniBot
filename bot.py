bb4a037785ac246d50d8f302f1fabc0b9c602ae0
max1808 <massimo.schembri18@gmail.com>
2017-09-27 15:56:13 +0200

    cambiata api telpot -> telebot

--- bot.py
+++ bot.py
@@ -19,26 +19,6 @@ def subscribe(message):
 
 
 
-#def handle(msg):
- #   content_type, chat_type, chat_id = telepot.glance(msg)
-  #  print(content_type, chat_type, chat_id)
-   # #riconoscimento input
-    #if content_type == 'text' and  msg['text'] == '/start':
-     #   util.add_id(chat_id)
-
-
-
-
 print("Polling...")
 
 bot.polling()
-
-
-
-#MessageLoop(bot, handle).run_as_thread()
-#print ('Listening ...')
-
-# Keep the program running.
-#while 1:
- #   time.sleep(10)
-
