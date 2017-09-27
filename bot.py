ed6788416f960a632581889a699725d4084f17d0
max1808 <massimo.schembri18@gmail.com>
2017-09-27 16:35:56 +0200

    Aggiunti handler per start,stop,domani

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import telebot
 import os
 import infounibot.util as util
+import infounibot.google_cal as cal
 
 
 # Get the bot token fron the enviromental variables
@@ -10,15 +11,27 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 str_welcome = 'Benvenuto nel fantastico UniBot'
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start','Start'])
 def subscribe(message):
     id = message.chat.id
     print(str(id))
     util.add_id(id)
     bot.reply_to(message,str_welcome)
 
+@bot.message_handler(commands=['stop','Stop'])
+def unsub(message):
+    id = message.chat.id
+    util.remove_id(id)
+    bot.reply_to(message,"Non riceverai più nessuna notifica.\n Grazie e alla prossima volta!.")
 
+@bot.message_handler(commands=['domani','Domani'])
+def send_tomorrow(message):
+    calendar = cal.CalendarReader()
+    calendar.load_events()
+    text = calendar.get_tomorrow_message()
+    print(str(text))
+    bot.send_message(message.chat.id,text)
 
 print("Polling...")
 
-bot.polling()
+bot.polling(none_stop=True)
