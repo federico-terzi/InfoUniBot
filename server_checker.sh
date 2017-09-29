6608c54391581e75c3b5c085b951f7a8676f12b4
Massimo Schembri <32139342+max1808@users.noreply.github.com>
2017-09-29 20:36:20 +0200

    Update server_checker.sh

--- server_checker.sh
+++ server_checker.sh
@@ -6,7 +6,7 @@ count=`ps -aux | grep bot.py | grep -v grep | wc -l`;
 
 if [[ $count == 0 ]] ; then
 	echo "Bot is not running, starting it...";
-	export bot_token=410200334:AAG3NN-rT2iUND5xbfqV62WDNPwJPDqmqXM && /usr/bin/python /home/freddy/bot/InfoUniBot/bot.py &
+	export bot_token=(?) && /usr/bin/python /home/freddy/bot/InfoUniBot/bot.py &
 else
 	echo "Allright, bot is running!";
 fi
